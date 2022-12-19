96f3b6187dab43f72ee00770a2101fd007d1e23f
Rachel Isaac <rachelisaac995@gmail.com>
2022-12-20 01:53:40 +0400

    Fix eslint errors

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
-const hamburger = document.querySelector(".hamburger");
-const mobileMenuBar = document.querySelector(".hamburger-menu-container");
-const navLinks = document.querySelectorAll(".nav-link");
+const hamburger = document.querySelector('.hamburger');
+const mobileMenuBar = document.querySelector('.hamburger-menu-container');
+const navLinks = document.querySelectorAll('.nav-link');
+
 function mobileMenu() {
-  hamburger.classList.toggle("active");
-  mobileMenuBar.classList.toggle("show-menu");
+  hamburger.classList.toggle('active');
+  mobileMenuBar.classList.toggle('show-menu');
 }
 
-hamburger.addEventListener("click", mobileMenu);
+hamburger.addEventListener('click', mobileMenu);
 
 function removeMenu() {
-  mobileMenuBar.classList.remove("show-menu");
-  hamburger.classList.remove("active");
+  mobileMenuBar.classList.remove('show-menu');
+  hamburger.classList.remove('active');
 }
 const navLinksArray = Array.from(navLinks);
 // eslint-disable-next-line no-plusplus
 for (let i = 0; i < navLinksArray.length; i++) {
-  navLinksArray[i].addEventListener("click", removeMenu);
+  navLinksArray[i].addEventListener('click', removeMenu);
 }
