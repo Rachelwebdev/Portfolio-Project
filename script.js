b6b8d9f080f8c6bbf4fe1578a8f08f57341bf6fd
Rachel Isaac <rachelisaac995@gmail.com>
2022-12-21 18:48:39 +0400

    Add desktop pop up window markup and styles

--- script.js
+++ script.js
@@ -218,86 +218,102 @@ function creatMobilePopup() {
 }
 creatMobilePopup();
 
+const projectNameM = document.querySelector(".popup-project-title");
+const projectImageM = document.querySelector(".project-image");
+const descriptionM = document.querySelector(".project-description");
+const technologies1M = document.querySelector(".ruby-technology");
+const technologies2M = document.querySelector(".css-technology");
+const technologies3M = document.querySelector(".javascript-technology");
+const liveLinkM = document.querySelector(".live-link");
+const sourceLinkM = document.querySelector(".source-link");
+
+projectNameM.textContent = projectArray[0].projectName;
+projectImageM.setAttribute("src", projectArray[0].projectImageMobile);
+descriptionM.textContent = projectArray[0].projectDescription;
+technologies1M.textContent = projectArray[0].technologies1;
+technologies2M.textContent = projectArray[0].technologies2;
+technologies3M.textContent = projectArray[0].technologies3;
+liveLinkM.setAttribute("src", projectArray[0].liveLink);
+sourceLinkM.setAttribute("src", projectArray[0].sourceLink);
+
 // -------------DESKTOP POPUP-----------
 function createDesktopPopup() {
   startingPoint.insertAdjacentHTML(
     "afterbegin",
     `<div class="desktop-popup-container">
         <img
+          class="cancel-icon"
+          src="./images/cross-X-btn.svg"
+          alt="project display image"
+        />
+        <img
           class="popup-desktop-img"
           src="./images/popup-desktop-img.svg"
           alt="project display image"
         />
         <div class="heading-button-container">
-          <h3 class="popup-project-title">
-            Keeping track of hundreds of components
+          <h3 class="desktop-popup-project-title">            
           </h3>
           <div class="live-source-button-container">
             <a
-              class="btn-live live-link"
+              class="btn-live-desktop live-link-desktop"
               href="https://rachelwebdev.github.io/Portfolio-Project/"
               >See Live
               <span>
                 <img
-                  class="button-icons"
+                  class="button-icons-desktop"
                   src="./images/see-live-icon.svg"
                   alt="white live icon" /></span
             ></a>
             <a
-              class="btn-live source-link"
+              class="btn-live-desktop source-link-desktop"
               href="https://github.com/Rachelwebdev/Portfolio-Project"
-              >See Live
+              >See Source
               <span>
                 <img
-                  class="button-icons"
+                  class="button-icons-desktop"
                   src="./images/github-popup-icon.svg"
                   alt="white github icon" /></span
             ></a>
           </div>
         </div>
         <ul class="popup-technologies">
-          <li class="Codekit-technology">Codekit</li>
-          <li class="GitHub-technology">GitHub</li>
-          <li class="Javascript-technology">Javascript</li>
-          <li class="Bootstrap-technology">Bootstrap</li>
-          <li class="Terminal-technology">Terminal</li>
-          <li class="Codepen-technology">Codepen</li>
+          <li class="Codekit-technology"></li>
+          <li class="GitHub-technology"></li>
+          <li class="js-technology"></li>
+          <li class="Bootstrap-technology"></li>
+          <li class="Terminal-technology"></li>
+          <li class="Codepen-technology"></li>
         </ul>
-        <p class="project-description"></p>
+        <p class="desktop-project-description"></p>
       </div>`
   );
 }
 createDesktopPopup();
 
-const projectName = document.querySelector(".popup-project-title");
-const projectImageMobile = document.querySelector(".project-image");
-const projectImageDesktop = document.querySelector(".popup-desktop-img");
-const description = document.querySelector(".project-description");
-const technologies1 = document.querySelector(".ruby-technology");
-const technologies2 = document.querySelector(".css-technology");
-const technologies3 = document.querySelector(".javascript-technology");
+const projectNameD = document.querySelector(".desktop-popup-project-title");
+const projectImageD = document.querySelector(".popup-desktop-img");
+const descriptionD = document.querySelector(".desktop-project-description");
+const technologies3D = document.querySelector(".js-technology");
 const technologies4 = document.querySelector(".Codekit-technology");
 const technologies5 = document.querySelector(".GitHub-technology");
 const technologies6 = document.querySelector(".Bootstrap-technology");
 const technologies7 = document.querySelector(".Terminal-technology");
 const technologies8 = document.querySelector(".Codepen-technology");
-const liveLink = document.querySelector(".live-link");
-const sourceLink = document.querySelector(".source-link");
+const liveLinkD = document.querySelector(".live-link-desktop");
+const sourceLinkD = document.querySelector(".source-link-desktop");
 
-projectName.textContent = projectArray[0].projectName;
-projectImageMobile.setAttribute("src", projectArray[0].projectImageMobile);
-projectImageDesktop.setAttribute("src", projectArray[0].projectImageDesktop);
-description.textContent = projectArray[0].projectDescription;
-technologies1.textContent = projectArray[0].technologies1;
-technologies2.textContent = projectArray[0].technologies2;
-technologies3.textContent = projectArray[0].technologies3;
+projectNameD.textContent = projectArray[0].projectName;
+projectImageD.setAttribute("src", projectArray[0].projectImageDesktop);
+descriptionD.textContent = projectArray[0].projectDescription;
+technologies3D.textContent = projectArray[0].technologies3;
 technologies4.textContent = projectArray[0].technologies4;
 technologies5.textContent = projectArray[0].technologies5;
 technologies6.textContent = projectArray[0].technologies6;
 technologies7.textContent = projectArray[0].technologies7;
 technologies8.textContent = projectArray[0].technologies8;
-liveLink.setAttribute("src", projectArray[0].liveLink);
-sourceLink.setAttribute("src", projectArray[0].sourceLink);
+liveLinkD.setAttribute("src", projectArray[0].liveLink);
+sourceLinkD.setAttribute("src", projectArray[0].sourceLink);
 
 // -----------ITERATE OVER THE MAIN PROJECT ARRAY--------------
 // for (let i = 0; i < projectArray.length; i++) {
