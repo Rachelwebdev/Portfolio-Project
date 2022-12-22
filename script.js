3d6ae43ed377cba3c5601abe6f3585088cae5b9f
Rachel Isaac <rachelisaac995@gmail.com>
2022-12-22 19:09:05 +0400

    Fix eslint error

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function removeMenu() {
   hamburger.classList.remove('active');
 }
 const navLinksArray = Array.from(navLinks);
-for (let i = 0; i < navLinksArray.length; i+=1) {
+for (let i = 0; i < navLinksArray.length; i += 1) {
   navLinksArray[i].addEventListener('click', removeMenu);
 }
 
